4d2056247b1a07d76c78e67322b5de31f0b7016a
IlyaMescheryakov <ilya_mesheryakov_1999@mail.ru>
2021-10-09 00:54:34 +0300

    test commit

--- tools/analyze_script.py
+++ tools/analyze_script.py
@@ -3,6 +3,7 @@ import os
 def analyze():
     with open('validation.csv', 'w') as f_obj:
         f_obj.write('ya zapuskayus')
+        # изменение, которое я хочу увидеть в коммитах
     # filename, file_extension = os.path.splitext('/path/to/somefile.ext')
 
 if __name__ == "__main__":

816f9b8c16bcfed6fd0f704343b6e2cff22d34ce
IlyaMescheryakov <ilya_mesheryakov_1999@mail.ru>
2021-10-10 15:15:08 +0300

    dev time

--- tools/analyze_script.py
+++ tools/analyze_script.py
@@ -2,7 +2,7 @@ import os
 
 def analyze():
     with open('artifacts/analyze_artifact.csv', 'w+') as f_obj:
-        f_obj.write('ya zapuskayus - 10.10')
+        f_obj.write('ya zapuskayus - analyze 10.10 15:14')
         # изменение, которое я хочу увидеть в коммитах
     # filename, file_extension = os.path.splitext('/path/to/somefile.ext')
 
--- tools/stats_script.py
+++ tools/stats_script.py
@@ -2,7 +2,7 @@ import os
 
 def analyze():
     with open('artifacts/stats_artifact.csv', 'w+') as f_obj:
-        f_obj.write('ya zapuskayus - statistics 10.10')
+        f_obj.write('ya zapuskayus - statistics 10.10 15:14')
     # filename, file_extension = os.path.splitext('/path/to/somefile.ext')
 
 if __name__ == "__main__":

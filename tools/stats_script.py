964be4370a133dcec7dd5485d79f3bba44e8049d
IlyaMescheryakov <ilya_mesheryakov_1999@mail.ru>
2021-10-10 19:48:54 +0300

    dev

--- tools/stats_script.py
+++ tools/stats_script.py
@@ -1,18 +1,23 @@
 import os
+import csv
 
 path="data"
 
 def analyze():
     with open('artifacts/stats_artifact.csv', 'w+') as f_obj:
-        f_obj.write('ya zapuskayus - statistics 10.10 15:14')
+        # f_obj.write('ya zapuskayus - statistics 10.10 15:14')
+        csv_writer = csv.writer(f_obj, delimiter=",")
         for root, dirs, files in os.walk(path):
             for file in files:
-                # print(f"name = {}, extension = {}".format(os.path.splitext(file)))
                 filename, file_extension = os.path.splitext(file)
-                print("name = {}, extension = {}".format(filename, file_extension))
-                # print(os.path.splitext(file))
+                if file_extension == '.wav':
+                    is_valid = 1
+                else:
+                    is_valid = 0
+                csv_writer.writerow([filename, is_valid])
+                print(filename+',', is_valid)
+                # print("name = {}, extension = {}".format(filename, file_extension))
 
-    # filename, file_extension = os.path.splitext('/path/to/somefile.ext')
 
 if __name__ == "__main__":
     analyze()

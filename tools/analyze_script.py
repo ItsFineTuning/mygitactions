ffe5017868fa3ce6451b09e579598890cba909f1
IlyaMescheryakov <ilya_mesheryakov_1999@mail.ru>
2021-10-10 14:02:33 +0300

    dev

--- tools/analyze_script.py
+++ tools/analyze_script.py
@@ -1,7 +1,7 @@
 import os
 
 def analyze():
-    with open('analyze_artifact.csv', 'w+') as f_obj:
+    with open('artifacts/analyze_artifact.csv', 'w+') as f_obj:
         f_obj.write('ya zapuskayus - 10.10')
         # изменение, которое я хочу увидеть в коммитах
     # filename, file_extension = os.path.splitext('/path/to/somefile.ext')
--- tools/stats_script.py
+++ tools/stats_script.py
@@ -1,7 +1,7 @@
 import os
 
 def analyze():
-    with open('stats_artifact.csv', 'w+') as f_obj:
+    with open('artifacts/stats_artifact.csv', 'w+') as f_obj:
         f_obj.write('ya zapuskayus - statistics 10.10')
     # filename, file_extension = os.path.splitext('/path/to/somefile.ext')
 

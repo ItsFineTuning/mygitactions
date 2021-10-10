a3bcf6cad805a445b89f04768ec5ee1c7ba8eac5
IlyaMescheryakov <ilya_mesheryakov_1999@mail.ru>
2021-10-10 20:33:33 +0300

    main dev changes

--- tools/stats_script.py
+++ tools/stats_script.py
@@ -1,24 +1,21 @@
 import os
 import csv
+import soundfile as sf
 
 path="data"
 
-def analyze():
+def stats():
     with open('artifacts/stats_artifact.csv', 'w+') as f_obj:
-        # f_obj.write('ya zapuskayus - statistics 10.10 15:14')
         csv_writer = csv.writer(f_obj, delimiter=",")
         for root, dirs, files in os.walk(path):
             for file in files:
-                filename, file_extension = os.path.splitext(file)
-                if file_extension == '.wav':
-                    is_valid = 1
-                else:
-                    is_valid = 0
-                csv_writer.writerow([filename, is_valid])
-                print(filename+',', is_valid)
-                # print("name = {}, extension = {}".format(filename, file_extension))
+                data, sr = sf.read(f"data/{file}")
+                filename, _ = os.path.splitext(file)
+                csv_writer.writerow([filename, data.shape[1], sr])
+                print(filename+',', sr+',', data.shape[1])
 
 
 if __name__ == "__main__":
-    analyze()
-    # print('Hello world!')
+    print('Start collecting statistic:\n')
+    stats()
+    print('\nCollection ended')

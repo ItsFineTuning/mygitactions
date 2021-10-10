d105808032814f66dac02cf2846649ae6e1a941f
IlyaMescheryakov <ilya_mesheryakov_1999@mail.ru>
2021-10-10 19:13:26 +0300

    dev

--- tools/stats_script.py
+++ tools/stats_script.py
@@ -7,7 +7,10 @@ def analyze():
         f_obj.write('ya zapuskayus - statistics 10.10 15:14')
         for root, dirs, files in os.walk(path):
             for file in files:
-                print(file)
+                # print(f"name = {}, extension = {}".format(os.path.splitext(file)))
+                filename, file_extension = os.path.splitext(file)
+                print("name = {}, extension = {}".format(filename, file_extension))
+                # print(os.path.splitext(file))
 
     # filename, file_extension = os.path.splitext('/path/to/somefile.ext')
 

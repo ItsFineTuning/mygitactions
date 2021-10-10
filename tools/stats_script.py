8d118134b15945985b93e459812a8715285711cd
IlyaMescheryakov <ilya_mesheryakov_1999@mail.ru>
2021-10-10 19:03:19 +0300

    dev stats

--- tools/stats_script.py
+++ tools/stats_script.py
@@ -1,8 +1,14 @@
 import os
 
+path="data"
+
 def analyze():
     with open('artifacts/stats_artifact.csv', 'w+') as f_obj:
         f_obj.write('ya zapuskayus - statistics 10.10 15:14')
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                print(file)
+
     # filename, file_extension = os.path.splitext('/path/to/somefile.ext')
 
 if __name__ == "__main__":

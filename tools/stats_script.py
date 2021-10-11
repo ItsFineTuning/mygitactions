a90cd4680ad48036d2fd61c40463ea99afcbbd35
IlyaMescheryakov <ilya_mesheryakov_1999@mail.ru>
2021-10-11 19:41:46 +0300

    dev

--- tools/stats_script.py
+++ tools/stats_script.py
@@ -1,4 +1,5 @@
 import csv
+import hashlib
 import os
 import pandas
 import soundfile
@@ -11,6 +12,7 @@ def stats():
         for root, dirs, files in os.walk(path):
             for file in files:
                 print(soundfile.available_formats())
+                print(hashlib.md5(file.encode()).hexdigest())
                 data, sr = soundfile.read(f"./{path}/{file}")
                 filename, _ = os.path.splitext(file)
                 csv_writer.writerow([filename, sr, data.shape[1]])
